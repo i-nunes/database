61382aab9c8e9c6ec3cf0e1aff3c50180cbf3f44
inunes2mundos <iurinunes01@gmail.com>
2020-12-02 14:28:28 -0300

    personal blog activity

---
+++ springboot/blogPessoal/BlogPessoal/src/main/java/com/example/demo/controller/PostagemController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Postagem;
+import com.example.demo.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> findAllPostagem() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/postagem/{id}")
+	public ResponseEntity<Postagem> findByIDPostagem(@PathVariable Long id) {
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+}
---
+++ springboot/blogPessoal/db_blogPessoal.sql
@@ -0,0 +1,4 @@
+USE blog_pessoal;
+SELECT * FROM blog_pessoal.postagem;
+INSERT INTO `blog_pessoal`.`postagem` (	`data`, `texto`, `titulo`)
+	VALUES ('2020-02-12 14:00:00.000', 'Estou aprendendo', 'API Rest Spring'), ('2020-02-12 14:00:00.000', 'Inserting new post', 'API Rest is very cool');
